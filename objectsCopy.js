9372522aa06856ca958a41d727a921763c6da231
carlosestu <164887215+carlosestu@users.noreply.github.com>
2024-04-23 17:11:39 +0200

    Update objectsCopy.js

--- objectsCopy.js
+++ objectsCopy.js
@@ -29,8 +29,8 @@ person2.firstName = "Carlos";
 console.log(person1);
 console.log(person2);
 
-// la razon por la cual tal y como estaba antes, al modificar person1, se modificaba tambien perdon2, 
-// es porque al tu poner person2=person1, person2 esta apuntando exactamente al mismo sitio en el que esta almacenado person1,
+// la razon por la cual tal y como estaba antes, al modificar person1, se modificaba tambien person2, 
+// es porque al poner person2=person1, person2 esta apuntando exactamente al mismo sitio en el que esta almacenado person1,
 // y por lo tanto los valores de person2 seran siempre los mismos que los de person1, tal y como lo he puesto yo, person2 se almacena
 // en un sitio nuevo, y con el bucle for in copia los valores de person1, pero con la diferencia de que al estar en distintos puntos de almacenamiento
 // permite cambiar los valores de uno, sin que se cambien tambien los del otro.
