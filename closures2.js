87814695237972f254cac830a2c2dfbd3dd32028
Carlos <carlosestupina@gmail.com>
2024-04-23 18:43:04 +0200

    ejercicio7-JS-Closures2

--- closures2.js
+++ closures2.js
@@ -1,11 +1,16 @@
 // Try to write your first closure. Write a function called printName and declare inside it a variable called helloName, 
 // the value of this variable will be: "Hello John". 
 // Also declare a function named inner and return the helloName variable. The purpose is printing "Hello John" in the console.
+// --------------------------------------------------------------------------------------------------------------------------------------------
+// Starting from the previous exercise, are you able to print the value of helloName, after 1 second?
 function printName(){
   let helloName = "Hello john";
+  
   function inner(){
-    return helloName;
+    setTimeout(() => {
+      console.log(helloName);
+    }, 1000);
   }
   return inner;
 }
-console.log(printName()())
+printName()()
