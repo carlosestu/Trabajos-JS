fe231e83fcc4b2784de0432c7fe1b00aaf3d3e30
Carlos <carlosestupina@gmail.com>
2024-04-23 14:17:28 +0200

    ejercicio2-JS-FuncNickname

---
+++ funcNickname.js
@@ -0,0 +1,24 @@
+function nicknameMap(personas) {
+    let apodos = [];
+    for (let i = 0; i < personas.length; i++) {
+        let person= persons[i].name + ", "+ persons[i].age;
+        apodos.push(person);
+    }
+    return apodos;
+}
+  const persons = [
+    { name: 'Paul', age: 16 },
+    { name: 'George', age: 17 },
+    { name: 'Lucas', age: 21 },
+    { name: 'Marco', age: 32 },
+    { name: 'Peter', age: 18 },
+    { name: 'Carl', age: 13 },
+    { name: 'Simon', age: 24 },
+    { name: 'Mark', age: 15 },
+    { name: 'Sandra', age: 34 },
+    { name: 'Alice', age: 28 }
+  ];
+  
+  const nicknames = nicknameMap(persons);
+  console.log(persons);
+  console.log(nicknames);
